d68723c34262874c2ddd84c0f7c75619531d59b8
ergunDevv <ergncyln2127088@gmail.com>
2022-11-10 15:03:36 +0300

    Initial Project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/auth";
+
+import {useAuthState} from "react-firebase-hooks/auth"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
